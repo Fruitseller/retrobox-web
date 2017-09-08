0db94143d49fbf75eff108d26b2167cd97132653
Peter Bronkalla <peterbronkalla@gmail.com>
2017-09-08 19:17:01 +0200

    Add dependencies and reformat code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import AddRetroItem from './components/AddRetroItem';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <AddRetroItem/>
+        <AddRetroItem />
       </div>
     );
   }
--- src/components/AddRetroItem.js
+++ src/components/AddRetroItem.js
@@ -1,22 +1,26 @@
 import React from 'react';
-import '../AddRetroItem.css'
+import '../AddRetroItem.css';
 
 class AddRetroItem extends React.Component {
+  onChange = values => {
+    console.log(values);
+  };
 
-    onChange = (values) => {
-        console.log(values);
-    }
-
-    render() {
-        return (
-            <div className="input">
-                <div className="input-container">
-                   <input onChange={this.onChange} type="text" className="input-field" placeholder="Enter your retro thoughts" />
-                    <div className="input-field-shadow"></div>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="input">
+        <div className="input-container">
+          <input
+            onChange={this.onChange}
+            type="text"
+            className="input-field"
+            placeholder="Enter your retro thoughts"
+          />
+          <div className="input-field-shadow" />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default AddRetroItem;
