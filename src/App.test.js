8c1b75fa70ac118eb500b4af6234129aba115cc8
Peter Bronkalla <peterbronkalla@gmail.com>
2017-09-26 22:01:06 +0200

    Fix test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import raf from 'raf';
 import App from './App';
 
+configure({ adapter: new Adapter() });
+
+raf.polyfill();
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  shallow(<App />);
 });
