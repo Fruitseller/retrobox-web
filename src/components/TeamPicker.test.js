490f938ecbde1867ad0c8b1ae3f95cfb4a15b004
Peter Bronkalla <peterbronkalla@gmail.com>
2017-10-08 19:50:36 +0200

    Add simple test for TeamPicker
    Also remove raf dependency since its not needed

--- src/components/TeamPicker.test.js
+++ src/components/TeamPicker.test.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import App from './App';
+import TeamPicker from './TeamPicker';
 
 configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
-  shallow(<App />);
+  shallow(<TeamPicker />);
 });
