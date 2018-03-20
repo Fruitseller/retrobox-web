b96d934bc4f460f9a3d608dad2d056b39b22090a
Peter Bronkalla <peterbronkalla@gmail.com>
2018-03-20 20:38:44 +0100

    Remove ServiceWorker

--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'typeface-roboto';
 import App from './App';
-import { unregister } from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-unregister();
